d267ab594bf70b18a212b13ee8ffd57e23fe77d9
Anzhelika Kuchma <anzhelikakuchma@anzhelikas-air.home>
2024-02-13 15:59:33 -0330

    Reading html files from views folder

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
 const http = require('http');
+const routes = require('./routes.js')
  
 global.DEBUG = true;
  
 const server = http.createServer((req, res) =>{
     if(DEBUG) console.log(req.method+", "+req.url)
+
+    
+
+    let path = './views/'
     if(req.method === 'POST'){
  
     }else{
         switch(req.url){
             case '/':
-                console.log("home page");
+                path += 'index.html';
+                routes.indexPage(path, res);
                 break;
             case '/about':
-                console.log("about page");
+                path += 'about.html';
+                routes.aboutPage(path, res);
                 break;
-            case '/contact':
-                console.log("contatcs page");
+            case '/contacts':
+                path += 'contacts.html';
+                routes.constactsPage(path, res);
                 break;
             case '/products':
-                console.log("products page");
+                path += 'products.html';
+                routes.productsPage(path, res);
                 break;
             case '/subscribe':
-                console.log("subscribe page");
+                path += 'subscribe.html';
+                routes.subscribePage(path, res);
                 break;
             default:
                 console.log('404 Not Found');
                 break;
         }
     }
-})
- 
+
+});
+
 server.listen(3000, () => {
     console.log('Server is running...');
 });
